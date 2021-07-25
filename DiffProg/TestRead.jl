99e98e833d2bf2e37f6ac67b68d4c6196cbf3745
Surya Chakrabarti <suryapratim.chakrabarti@gmail.com>
2021-07-24 21:24:00 -0400

    getting the io sorted

--- DiffProg/TestRead.jl
+++ DiffProg/TestRead.jl
@@ -2,37 +2,43 @@ using DelimitedFiles
 
 
 
-# Reading from the publicData
-nInst=20;
-nSys=12;
+
+function systIO(NSyst,NInst)
+
+    # Reading from the publicData
+    # nInst=20;
+    # nSys=12;
 
 
-path="./../public_data/"
-fname="system_01_instance_00.csv"
+    path="./../public_data/"
 
-data=readdlm(string(path,fname),',',header=true)
-system_data=data[1][:,3:end]
-system_data[system_data.==""].=0.0;
-headers=data[2]
+    fname=string("system_",NSyst,"_instance_",NInst,".csv")
+    # fname="system_01_instance_00.csv"
 
-# count number of inputs
+    data=readdlm(string(path,fname),',',header=true)
+    system_data=data[1][:,3:end]
+    system_data[system_data.==""].=0.0;
+    headers=data[2]
+
+    # count number of inputs
 
 
-for varCtr in 1:1:length(headers)
-    if headers[varCtr] == "X1"
-        XSt=varCtr;
-        break;
-    end
-end
-
-ctr=0;
-for varCtr in 1:1:length(headers)
-    if headers[varCtr+XSt-1] == string("X",varCtr)
-        ctr+=1;
-    else
-        break
+    for varCtr in 1:1:length(headers)
+        if headers[varCtr] == "X1"
+            XSt=varCtr;
+            break;
+        end
     end
 
+    ctr=0;
+
+    for varCtr in 1:1:length(headers)
+        if headers[varCtr+XSt-1] == string("X",varCtr)
+            ctr+=1;
+        else
+            break
+        end
+    end
+
+
 end
-
-
